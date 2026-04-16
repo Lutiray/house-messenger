99ee585bea99ebf70932c14c2df5e123f6e86034
Natalia Eremina <entalia663@gmail.com>
2026-04-16 17:14:34 +0200

    use findDirectChannel in getChatHistory to avoid empty channel creation

--- messenger-server/include/db/database_manager.hpp
+++ messenger-server/include/db/database_manager.hpp
@@ -17,8 +17,8 @@ public:
 
     json parseMessageRow(sqlite3_stmt *stmt);
     json getDialogsList(const std::string& my_nick) override;
-    json getChatHistory(const std::string &my_nick, const std::string &other_nick) override;
-    
+    json getChatHistory(const std::string &my_nick, const std::string &other_nick, int before_id) override;
+
     bool registerUser(const std::string &username, const std::string &password, 
         const std::string &email, const std::string &phone) override;
     bool checkAuth(const std::string& username, const std::string& password) override;
@@ -26,6 +26,7 @@ public:
     bool editMessage(int msg_id, const std::string& owner_nick, const std::string& new_text) override;
     bool markChatAsRead(const std::string &sender_nick, const std::string &receiver_nick) override;
     int getOrCreateDirectChannel(int user1_id, int user2_id);
+    int findDirectChannel(int user1_id, int user2_id);
     json searchUsers(const std::string &query) override;
     int saveMessage(const std::string &sender_nick, const std::string &text, const std::string &receiver_nick) override;
     int getUserId(const std::string& username) override;
@@ -34,7 +35,8 @@ private:
     sqlite3* _db;
     std::mutex _db_mutex;
     std::string _path;
-    std::string hashPassword(const std::string& password);
+    std::string generateSalt();
+    std::string hashPassword(const std::string& password, const std::string& salt);
     int getUserIdInternal(const std::string &username);
 };
 
