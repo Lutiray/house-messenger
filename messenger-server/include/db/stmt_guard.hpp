916cfb0c59b436bd00929676c60db0b242d60b1a
Natalia Eremina <entalia663@gmail.com>
2026-04-28 15:04:21 +0200

    Format code for better readability

--- messenger-server/include/db/stmt_guard.hpp
+++ messenger-server/include/db/stmt_guard.hpp
@@ -1,25 +1,28 @@
 #pragma once
 #include <sqlite3.h>
- 
-class StmtGuard {
+
+class StmtGuard
+{
 public:
     StmtGuard() : _stmt(nullptr) {}
- 
-    ~StmtGuard() {
-        if (_stmt) sqlite3_finalize(_stmt);
+
+    ~StmtGuard()
+    {
+        if (_stmt)
+            sqlite3_finalize(_stmt);
     }
- 
-    StmtGuard(const StmtGuard&)            = delete;
-    StmtGuard& operator=(const StmtGuard&) = delete;
- 
-    sqlite3_stmt** ptr() { return &_stmt; }
- 
-    operator sqlite3_stmt*() { return _stmt; }
+
+    StmtGuard(const StmtGuard &) = delete;
+    StmtGuard &operator=(const StmtGuard &) = delete;
+
+    sqlite3_stmt **ptr() { return &_stmt; }
+
+    operator sqlite3_stmt *() { return _stmt; }
 
     int step() { return sqlite3_step(_stmt); }
- 
+
     bool is_valid() const { return _stmt != nullptr; }
- 
+
 private:
-    sqlite3_stmt* _stmt;
+    sqlite3_stmt *_stmt;
 };
